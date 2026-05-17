b2a5ebfce5a665a34ace212f5274356db8401f54
GH15339 <why20231127@gmail.com>
2026-05-17 15:21:14 +0800

    調整

--- executor-wasm/script.js
+++ executor-wasm/script.js
@@ -18,7 +18,7 @@ const callback = function(mutationsList, observer) {
                         // document.querySelector("canvas").style.cssText = "width: 800px; height: 600px;";
                         const canvas = document.querySelector("canvas");
                             window.addEventListener('resize', () => {
-                            canvas.style.cssText = `width: 100%; height: 100%;`;
+                            canvas.style.cssText = `width: ${document.documentElement.clientWidth}px; height: ${document.documentElement.clientHeight}px;`;
                             canvas.setAttribute('width', canvas.clientWidth);
                             canvas.setAttribute('height', canvas.clientHeight);
                         });
