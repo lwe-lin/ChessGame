045366b2f51e871ef538727db0a7de5a1964e786
GH15339 <why20231127@gmail.com>
2026-05-17 14:46:02 +0800

    調整獲取寬度跟高度方式

--- executor-wasm/script.js
+++ executor-wasm/script.js
@@ -18,9 +18,9 @@ const callback = function(mutationsList, observer) {
                         // document.querySelector("canvas").style.cssText = "width: 800px; height: 600px;";
                         const canvas = document.querySelector("canvas");
                             window.addEventListener('resize', () => {
-                            canvas.setAttribute('width', window.innerWidth);
-                            canvas.setAttribute('height', window.innerHeight);
-                            canvas.style.cssText = `width: ${window.innerWidth-200}px; height: ${window.innerHeight-150}px;`;
+                            canvas.setAttribute('width', document.documentElement.clientWidth);
+                            canvas.setAttribute('height', document.documentElement.clientHeight);
+                            canvas.style.cssText = `width: ${document.documentElement.clientWidth-200}px; height: ${document.documentElement.clientHeight-150}px;`;
                         });
                     }
                 }
